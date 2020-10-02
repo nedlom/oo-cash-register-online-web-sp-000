a75c7e6c37d52a0b1c1386625eaec3911c5b8c0d
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-02 21:21:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,38 +1,95 @@
-require 'pry'
 class CashRegister
-
+  
   attr_accessor :total, :discount
-
+  
   def initialize(discount = nil)
     @total = 0
     @discount = discount
-    @items = []
+    @items = {}
   end
   
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
-    @items.concat([title] * quantity)
-    @last = price * quantity
+    self.items[title] = [price, quantity]
   end
-
+  
+  def items
+    items.keys
+  end
+  
   def apply_discount
-    if self.discount
-      self.total -= self.total * discount / 100
-      "After the discount, the total comes to $#{self.total}."
+    if discount
+      self.total -= total * discount / 100
+      "After the discount, the total comes to $#{total}."
     else
       "There is no discount to apply."
     end
   end
   
-  def items
-    @items
+  def void_last_transaction
+     self.total -= items[-1][0] * items[-1][1]
   end
   
-  def void_last_transaction
-    self.total -= @last
-  end
-
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'pry'
+# class CashRegister
+
+#   attr_accessor :total, :discount
+
+#   def initialize(discount = nil)
+#     @total = 0
+#     @discount = discount
+#     @items = []
+#   end
+  
+#   def add_item(title, price, quantity = 1)
+#     self.total += price * quantity
+#     @items.concat([title] * quantity)
+#     @last = price * quantity
+#   end
+
+#   def apply_discount
+#     if self.discount
+#       self.total -= self.total * discount / 100
+#       "After the discount, the total comes to $#{self.total}."
+#     else
+#       "There is no discount to apply."
+#     end
+#   end
+  
+#   def items
+#     @items
+#   end
+  
+#   def void_last_transaction
+#     self.total -= @last
+#   end
+
+# end
   
 =begin
 
