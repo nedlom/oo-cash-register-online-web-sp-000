1aa00ebc7d057dfa71f0aaa435bacc38939195f5
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-02 21:42:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
   
-  attr_accessor :total, :discount, :items
+  attr_accessor :total, :discount, :items, :last
   
   def initialize(discount = nil)
     @total = 0
@@ -9,7 +9,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    @last_purchase = price * quantity
+    self.last = price * quantity
     self.total += price * quantity
     self.items += [title] * quantity
   end
@@ -24,83 +24,7 @@ class CashRegister
   end
   
   def void_last_transaction
-     self.total -= @last_purchase
+     self.total -= last
   end
   
 end
-
-
-# require 'pry'
-# class CashRegister
-
-#   attr_accessor :total, :discount
-
-#   def initialize(discount = nil)
-#     @total = 0
-#     @discount = discount
-#     @items = []
-#   end
-  
-#   def add_item(title, price, quantity = 1)
-#     self.total += price * quantity
-#     @items.concat([title] * quantity)
-#     @last = price * quantity
-#   end
-
-#   def apply_discount
-#     if self.discount
-#       self.total -= self.total * discount / 100
-#       "After the discount, the total comes to $#{self.total}."
-#     else
-#       "There is no discount to apply."
-#     end
-#   end
-  
-#   def items
-#     @items
-#   end
-  
-#   def void_last_transaction
-#     self.total -= @last
-#   end
-
-# end
-  
-=begin
-
-  attr_accessor :total, :discount
-  
-  def initialize(discount = nil)
-    @total = 0
-    @discount = discount
-    @items = []
-    @price = {}
-  end
-  
-  def add_item(title, price, quantity = 1)
-    @total += price * quantity
-    @price[title] = price*quantity
-    quantity.times do
-      @items << title
-    end
-  end
-  
-  def items
-    @items 
-  end
-  
-  def apply_discount
-    if discount
-      @total = @total - @discount * @total / 100
-      "After the discount, the total comes to $#{@total}."
-    else
-      "There is no discount to apply."
-    end
-  end
-  
-  def void_last_transaction
-      @total = @total - @price[@items[-1]]
-  end
-
-end
-=end
